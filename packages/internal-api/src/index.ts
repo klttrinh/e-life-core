937a05f30f076507e4c0bb2e77dfa816c5922e00
Khuu Le Thuc Trinh <tuan.nt34@oude.edu.vn>
2024-10-07 14:10:19 +0700

    fix: remove role type enum

--- packages/internal-api/src/index.ts
+++ packages/internal-api/src/index.ts
@@ -2,4 +2,3 @@ export * from './default.client.service';
 export * from './default.client.module';
 export * from './internal.http';
 export * from './enums/internal.events.enum';
-export * from './enums/role.type.enum';
